19833354c6b14754f762b0ec41e70b487c18e927
Aravind <aravind@Aravind.local>
2017-02-14 15:22:15 +0530

    initial commit

---
+++ neGitDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  neGitDemo
+//
+//  Created by Aravind on 14/02/17.
+//  Copyright © 2017 AravindBolasIntelli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ neGitDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  neGitDemo
+//
+//  Created by Aravind on 14/02/17.
+//  Copyright © 2017 AravindBolasIntelli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
